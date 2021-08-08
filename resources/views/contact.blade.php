7d22e1b70c0c67ba6834017a5af813b3c643379b
Andres Camilo Rodriguez Prieto <42007594+andresrcp@users.noreply.github.com>
2021-08-08 14:38:51 -0500

    Initial commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+@extends('layouts.templates')
+@section('title','Contacto')
---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.templates')
+<!-- page title -->
+@section('title','Inicio')
+<!-- content -->
+@section('content')
+    <h1 class="text-center">Inicio</h1>
+    <img src="{{URL::asset('images\header-bg.png')}}" class="img-fluid" alt="header-content-bg">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi dolore voluptatem enim minus illum, aperiam doloremque quas quos incidunt exercitationem quae molestiae quam laudantium provident hic et dignissimos praesentium obcaecati amet! Tenetur quam aut eaque illo alias tempore obcaecati hic beatae accusantium perspiciatis voluptatem iure suscipit porro quae voluptates, ea, soluta consequuntur consectetur tempora quo nesciunt quasi provident? Debitis dolores voluptate ab distinctio quo, placeat reprehenderit explicabo nulla, ea ad quod dicta tempore corrupti reiciendis quis, quia ut unde fugit? Asperiores commodi sequi error provident esse nesciunt deleniti, inventore corporis dignissimos repellat libero sapiente expedita numquam praesentium enim mollitia facere?</p>
+@endsection
